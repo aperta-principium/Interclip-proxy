a857b0a50e9f4be964e29e6311158232ffc23b87
Filip Troníček <filip.tronicek@seznam.cz>
2020-03-23 11:20:31 +0100

    Update redirect.js

--- js/redirect.js
+++ js/redirect.js
@@ -2,11 +2,18 @@ var url_string = window.location.href;
 var url = new URL(url_string);
 var code = url.searchParams.get("code");
 if (code != null) {
-  console.log(code);
-  location.href = "http://uni.hys.cz/r?c=" + code;
+  codeAr = code.split("");
+  if (codeAr.length == 5) {
+    console.log(code);
+    location.href = "http://uni.hys.cz/r?c=" + code;
+  } else {
+      document.write("A code false length assigned");   
+  }
 } else {
-    location.href = "http://uni.hys.cz/";
+  location.href = "http://uni.hys.cz/";
 }
-if (location.href.match(/^http:/i)) {
-  location.replace(location.href.replace(/^http:/i, 'https:'));
-}
+/*
+    if (location.href.match(/^http:/i)) {
+        location.replace(location.href.replace(/^http:/i, "https:"));
+      }
+*/      
